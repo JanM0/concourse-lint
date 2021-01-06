d2e10750834e8d44becdf9094aa86c3b791fab1a
JanM0 <janmollitor@web.de>
2021-01-06 11:50:29 +0100

    add maven project

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        linelength();
+        methodlength();
+    }
+
+    private static void methodlength() {
+        String a = "1";
+        a = "2";
+        a = "3";
+        a = "4";
+        a = "5";
+        a = "6";
+        a = "7";
+        a = "8";
+        a = "9";
+        a = "0";
+        a = "1";
+        a = "2";
+        a = "3";
+        a = "4";
+        a = "5";
+        a = "6";
+        a = "7";
+        a = "8";
+        a = "9";
+        a = "0";
+        a = "1";
+    }
+
+    private static void linelength() {
+        String a = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+    }
+}
